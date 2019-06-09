6dac948c0ba63153271cb2c890703949261f391f
zxlchina <zxlchina890824@163.com>
2019-06-09 21:18:35 +0800

    0.0.3

--- server/main.py
+++ server/main.py
@@ -79,11 +79,192 @@ def get_car_numbers_bd(img_path):
     return number_list
 
 
+
+def escape_str(s):
+    return s.replace("\\", "\\\\").replace("'", "\\'")
+    
+
+@app.route("/get_clist", methods=['GET', 'POST'])
+def get_clist():
+    sql = "select cid, cname from community_info"
+    result = get_result(sql)
+
+    if result is None:
+        res["ret"] = -1
+        res["error"] = "query db error!" 
+        return json.dumps(res)
+        
+
+    res = {}
+    res["ret"] = 0
+    res["clist"] = []
+    for item in result:
+        one = {}
+        one["cid"] = item[0]
+        one["cname"] = item[1]
+        res["clist"].append(one)
+
+    return json.dumps(res) 
+
+
+
+@app.route("/replay", methods=['GET', 'POST'])
+def replay():
+    content = escape_str(request.args.get('content', ''))
+    openid = escape_str(request.args.get('openid', ''))
+    sql = "insert into replay (content, author) values ('%s', '%s')" % (content, openid)
+
+    print(sql)
+
+
+    
+    result = get_result(sql)
+    
+    if result is None:
+        res = {}
+        res["ret"] = -1
+        res["error"] = "提交失败，请稍后再试"
+        return json.dumps(res)
+
+
+    res = {} 
+    res["ret"] = 0
+    return json.dumps(res) 
+    
+ 
+
+
+@app.route("/get_detail", methods=['GET', 'POST'])
+def get_detail():
+    car_number = request.args.get('car_number', '')
+    cid = int(request.args.get('cid', '')) 
+
+    sql = ""
+    if cid == 0 :
+        sql = "select create_time, img_url from cap_info where car_number = '%s' order by create_time desc" % (car_number)
+    else:
+        sql = "select create_time, img_url from cap_info where car_number = '%s' and cid = %d order by create_time desc" % (car_number, cid)
+    result = get_result(sql)
+
+
+    res = {}
+    res["ret"] = 0
+    res["detail"] = []
+    for item in result:
+        one = {}
+        one["create_time"] = item[0].strftime("%Y-%m-%d %H:%M:%S")
+        one["img_url"] = item[1]
+        res["detail"].append(one)
+
+    return json.dumps(res)
+        
+
+    
+
+@app.route("/get_user_count", methods=['GET', 'POST'])
+def get_user_count():
+    openid = escape_str(request.args.get('openid', ''))
+
+    if openid == "":
+        res = {}
+        res["ret"] = -1
+        res["error"] = "invalid openid"
+        return json.dumps(res)
+
+    
+    sql = "select count(distinct pub_id), count(distinct(if(create_time>=DATE_FORMAT( SUBDATE(CURDATE(),DATE_FORMAT(CURDATE(),'%%w')-1), '%%Y-%%m-%%d 00:00:00'), pub_id, null))) from cap_info where author = '%s'" % (openid)
+    sql = "select count(distinct pub_id), count(distinct(if(create_time>=DATE_FORMAT(curdate()- WEEKDAY(curdate()), '%%Y-%%m-%%d 00:00:00'), pub_id, null))) from cap_info where author = '%s'" % (openid)
+    #sql = "select count(distinct pub_id), count(distinct(if(create_time>=DATE_FORMAT( DATE_SUB(CURDATE(), INTERVAL 1 DAY), '%%Y-%%m-%%d 23:59:59'), pub_id, null))) from cap_info where author = '%s'" % (openid)
+
+    print(sql)
+    result = get_result(sql) 
+    if result is None:
+        res = {}
+        res["ret"] = -1
+        res["error"] = "query db error!"
+        return json.dumps(res) 
+
+    for item in result:
+        res = {}
+        print(item)
+        res["ret"] = 0
+        res["total_count"] = int(item[0])
+        res["week_count"] = int(item[1])
+        return json.dumps(res)
+
+    res = {}
+    res["ret"] = 0
+    res["total_count"] = 0 
+    res["week_count"] = 0 
+    return json.dumps(res)
+
+
+
+@app.route("/get_rank", methods=['GET', 'POST'])
+def get_rank():
+    cid = int(request.args.get('cid', '')) 
+
+    sql = ""
+    if cid == 0:
+        sql = "select car_number, count(1) as cnt from cap_info where car_number != ''  group by car_number order by cnt desc"
+    else:
+        sql = "select car_number, count(1) as cnt from cap_info where car_number != '' and cid=%d group by car_number order by cnt desc" % (cid)
+
+    result = get_result(sql)
+
+    if result is None:
+        res["ret"] = -1
+        res["error"] = "query db error!" 
+        return json.dumps(res)
+
+    res = {}
+    res["ret"] = 0
+    res["rank_list"] = []
+    
+    for item in result:
+        car = {}
+        car["number"] = item[0]
+        car["count"] = item[1]
+        res["rank_list"].append(car)
+    
+    return json.dumps(res)
+
+   
+
+
+@app.route("/update_user_info", methods=['GET', 'POST'])
+def update_user_info():
+    openid = escape_str(request.args.get('openid', ''))
+    nickname = escape_str(request.args.get('nickname', ''))
+    gender = escape_str(request.args.get('gender', ''))
+    avatar = escape_str(request.args.get('avatar', ''))
+    city = escape_str(request.args.get('city', ''))
+    country = escape_str(request.args.get('country', ''))
+    province = escape_str(request.args.get('province', ''))
+    lang = escape_str(request.args.get('lang', ''))
+
+    sql = "replace into user_info (openid, nickname, gender, avatar, city, country, province, lang) values ('%s', '%s', %s, '%s', '%s', '%s', '%s', '%s')" \
+     % (openid, nickname, gender, avatar, city, country, province, lang)
+    print(sql)
+
+
+    res = {}
+    res["ret"] = 0
+
+    result = get_result(sql)
+    if result is None:
+        res["ret"] = -1
+        res["error"] = "insert db error!" 
+
+    return json.dumps(res)
+
+
 @app.route("/add_item", methods=['GET', 'POST'])
 def add_item():
-    img = request.args.get('img', '')
-    openid = request.args.get('openid', '')
-    car_numbers = request.args.get('car_numbers', '')
+    img = escape_str(request.args.get('img', ''))
+    openid = escape_str(request.args.get('openid', ''))
+    car_numbers = escape_str(request.args.get('car_numbers', ''))
+    cid = escape_str(request.args.get('cid', ''))
 
     if img == "" :
         res = {}
@@ -98,19 +279,47 @@ def add_item():
         return json.dumps(res)
 
     
+
     numbers = json.loads(car_numbers) 
+
+    #生成pub_id
+    t = time.time()
+    pub_id = str((int(round(t * 1000))))
+
+    #检查是否需要写入空值
+    need_empty = True
+    for number in numbers:
+        if number != "":
+            #只要有一个非空的，则不需要写入空值
+            need_empty = False
+        
 
     img_url = urlparse(img)
     img_path = img_url.path
     
     sql = ""
+    has_insert = False
     for number in numbers:
-        sql="insert into cap_info (author, car_number, img_url) values ('%s', '%s', '%s')"  % (openid, number.upper(), img_path)
+        if number == "" :
+            if need_empty == False: #不需要保存空的
+                continue
+            if need_empty == True and has_insert == True: #需要保存空的，但是已经保存过了
+                continue
+
+        sql="insert into cap_info (author, car_number, img_url, pub_id, cid) values ('%s', '%s', '%s', '%s', %s)"  % (openid, number.upper(), img_path, pub_id, cid)
         print (sql)
+
+        result = get_result(sql)
+        if result is None:
+            app.logger.error("insert error:" + sql)
+            res = {}
+            res["ret"] = -1 
+            return json.dumps(res)
+
+        has_insert = True
     
     res = {}
-    res["ret"] = -1 
-    res["sql"] = sql
+    res["ret"] = 0 
     return json.dumps(res)
 
 
@@ -131,7 +340,6 @@ def code_2_session():
     res = requests.get(url)
     
     print(res.text)
-    time.sleep(1)
 
     return res.text
 
@@ -200,6 +408,9 @@ def upload():
         f.save(upload_file_path + filename)
 
         url = request.host_url + "static/" + filename
+        
+        #http转https,  否则图片预览时可能出现黑屏
+        url = url.replace("http:", "https:") 
 
         app.logger.debug("this is a log test")
 
